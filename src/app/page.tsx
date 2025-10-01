5ce9067ff0104f7238dfadb91840abecf87eebc3
RamaRaditya-s <misterfalcon.5512@gmail.com>
2025-10-01 18:09:20 +0700

    Deskripsi perubahan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,45 @@
+import { useState, useEffect } from "react";
 import AddPost from "@/components/AddPost";
 import Stories from "@/components/Stories";
 import Feed from "@/components/feed/Feed";
 import LeftMenu from "@/components/leftMenu/LeftMenu";
 import RightMenu from "@/components/rightMenu/RightMenu";
 
+// Skeletons
+import LeftMenuSkeleton from "@/components/skeleton/LeftMenuSkeleton";
+import StoriesSkeleton from "@/components/skeleton/StoriesSkeleton";
+import AddPostSkeleton from "@/components/skeleton/AddPostSkeleton";
+import FeedSkeleton from "@/components/skeleton/FeedSkeleton";
+import RightMenuSkeleton from "@/components/skeleton/RightMenuSkeleton";
+
 const Homepage = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // simulasi fetch data 2 detik
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="flex gap-6 pt-6">
+      {/* LeftMenu */}
       <div className="w-[20%]">
-        <LeftMenu type="home" />
+        {loading ? <LeftMenuSkeleton /> : <LeftMenu type="home" />}
       </div>
+
+      {/* Middle Content */}
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
-          <Stories />
-          <AddPost />
-          <Feed />
+          {loading ? <StoriesSkeleton /> : <Stories />}
+          {loading ? <AddPostSkeleton /> : <AddPost />}
+          {loading ? <FeedSkeleton /> : <Feed />}
         </div>
       </div>
+
+      {/* RightMenu */}
       <div className="hidden lg:block w-[30%]">
-        <RightMenu />
+        {loading ? <RightMenuSkeleton /> : <RightMenu />}
       </div>
     </div>
   );
